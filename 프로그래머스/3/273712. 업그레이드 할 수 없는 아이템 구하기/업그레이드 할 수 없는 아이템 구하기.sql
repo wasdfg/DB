c4ee6ac6694a87e4fb1194a9b62224113b247db3
wasdfg <leejh9711@naver.com>
2024-09-22 17:09:02 +0900

    Delete 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기 directory

--- 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
+++
@@ -1,6 +0,0 @@
-select a.ITEM_ID as ITEM_ID,ITEM_NAME,RARITY 
-from item_info as a left join item_tree as b on a.item_id = b.item_id
-where a.item_id not in 
-(select item_id from item_tree where item_id in 
- (select parent_item_id from item_tree where parent_item_id is not null))
-order by a.item_id desc;
